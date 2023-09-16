c9e46e99392634ef5e2f1e0be85884bc6ddb2178
Aesha7 <paesha27@gmail.com>
2023-09-16 18:04:06 -0400

    finished design for body shape screen

--- color-fit/components/BodyType.jsx
+++ color-fit/components/BodyType.jsx
@@ -9,9 +9,9 @@ const poppins = Poppins({
 
 export default function BodyType({ text, image }) {
   return (
-    <div className='w-96 h-1/4 bg-brown-300 flex flex-col p-6 m-2 justify-center items-center border-2 rounded-lg border-grey-700 grow'>
+    <div className='w-48 w-1/5 h-1/4 bg-brown-300 flex flex-col p-6 m-2 justify-center items-center border-2 rounded-lg'>
       <Image src={image} alt={text} height={250}/>
-      <p className={`${poppins.className} text-2xl font-mono p-4`}>{text}</p>
+      <p className={`${poppins.className} text-xl font-mono p-4`}>{text}</p>
     </div>
   )
 }
--- color-fit/pages/choice.js
+++ color-fit/pages/choice.js
@@ -10,29 +10,34 @@ import Hourglass from './assets/hour.png'
 export default function Choice() {
   return (
     <div>
-      <div className='text-center text-4xl font-bold'>Which body type best describes you?</div>
+      <div className='text-center text-4xl font-bold text-[#d9dede] bg-[#494A43] p-12 '>Which body type best describes you?</div>
       {/* row of choices */}
-      <div className='flex flex-row justify-between items-center py-12 px-4'>
+      <div className='flex flex-row justify-between items-center py-12 px-4 rounded-2xl mx-8'>
+        <div className='hover:bg-[#EFDCD0] rounded-2xl border-[#494A43] text-[#494A43]'> 
         <BodyType 
           image={Apple} 
           text="Apple"
-        />
+        /> </div>
+        <div className='hover:bg-[#EAD0Ad] rounded-2xl border-[#494A43] text-[#494A43]'> 
         <BodyType 
           image={Pear} 
           text="Pear"
-        />
-        <BodyType 
+        /> </div>
+      <div className='hover:bg-[#956D44] rounded-2xl border-[#494A43] hover:text-white'> 
+        <BodyType
           image={Inverted} 
           text="Inverted"
-        />
+        /> </div>
+      <div className='hover:bg-[#684032] rounded-2xl border-[#494A43] hover:text-white'> 
         <BodyType 
           image={Rectangular} 
           text="Rectangular"
-        />
+        /> </div>
+      <div className='hover:bg-[#352B25] rounded-2xl border-[#494A43] hover:text-white'> 
         <BodyType 
           image={Hourglass} 
           text="Hourglass"
-        />
+        /> </div>
       </div>
     </div>
   )
