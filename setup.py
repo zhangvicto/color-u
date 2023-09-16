91322d79275836ee779f3029c5bbc08b7db9e53d
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2023-09-16 03:04:23 -0400

    Update

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 import cv2 
+import numpy as np
+import webcolors
 
 # Human Identification
 def vision(): 
@@ -11,9 +13,24 @@ def vision():
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Detect faces
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+    skin_tone = []
     # Draw rectangle around the faces
     for (x, y, w, h) in faces:
+        # Patch Properties
+        patch_size = 5
+        offset = 20
+        patch = img[x+offset:x+offset+patch_size, y+offset:y+offset+patch_size]
+
+        # Avg colors in patch
+        avg_tone = np.mean(patch, axis=0)
+        skin_tone.append(avg_tone)
+
+        # Draw Rectangle for person
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        tone_name = webcolors.rgb_to_name(int(avg_tone))
+        cv2.putText(img, "Skin Tone {}".format(tone_name), (x, y), 0, 0.5, (0,0,255))
+        
     # Display the output
     cv2.imshow('img', img)
     cv2.waitKey()
