0bd7a4cd7039afc0b6f0d898ef6b9957e2951bd7
Aesha7 <paesha27@gmail.com>
2023-09-16 21:50:53 -0400

    finished design for body shape screen

--- color-fit/pages/taskbar.js
+++ color-fit/pages/taskbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Taskbar = ({ onNext, onPrevious }) => {
   return (
-    <div className="fixed bottom-2 left-0 right-0 p-4 flex justify-between">
+    <div className="fixed bottom-0 left-0 right-0 p-4 flex justify-between">
       <button onClick={onPrevious} className="prevbutton inline-block px-4 py-2 bg-[#d9ddde] ml-10 text-[#494a43] border-2 border-[#494a43] rounded-2xl hover:bg-[#494a43] hover:text-[#d9dede] focus:outline-none focus:ring focus:ring-[#edeeef] mx-auto font-medium">
         Previous
       </button>
